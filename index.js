dcf44e83d36e05783b0e5b0cb1ab93476d4ec5b5
gsiwatch <gaurishsiwatch@quickenloans.com>
2021-01-18 21:43:53 -0500

    custom lifecycles

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 class Tooltip extends HTMLElement {
     constructor() {
         super();
-        console.log('custom component basic setup');
+    }
+
+    connectedCallback() {
+        const tooltip = document.createElement('span');
+        tooltip.textContent = 'trying connectedCallback lifeCycle method';
+        this.appendChild(tooltip);
     }
 }
 
 customElements.define('gs-tooltip', Tooltip); // note: name has to be two words
+
+/**
+ * custom components lifeCycles while creating the custom components
+ * executes in the following order:
+ *    1. constructor()  -- Basic Initialization, wrong place for accessing the DOM \
+ *                          as custom element is not been added to the DOM yet
+ *    2. connectedCallback() -- this is called when custom element is connected to the DOM
+ *    3. disconnectedCallback() -- this will be called when custom element will be detached from the DOM
+ *    4. attributeChangedCallback() -- this will be called when attributes will be changed
+ *                                  -- used when updating Data + DOM
+ *    
+ */
