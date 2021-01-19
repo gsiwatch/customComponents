13438f0abb02d9bb37eaa56820a4ce4beff13432
gsiwatch <gaurish.siwatch@gmail.com>
2021-01-19 17:25:26 -0500

    adding body

--- index.js
+++ index.js
@@ -1,25 +1,35 @@
-class Tooltip extends HTMLElement {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class EmailConfirmation extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
-        const tooltip = document.createElement('span');
-        tooltip.textContent = 'trying connectedCallback lifeCycle method';
-        this.appendChild(tooltip);
+        const bodyMessage = document.createElement('p');
+        bodyMessage.textContent = `We've sent you an email from no-reply@RocketAccount.com
+        so we can verify your email address. Just click the button in the email 
+        to finish creating your account. If you don't receive the email, plase check your spam or junk folder.`;
+        this.appendChild(bodyMessage);
     }
 }
 
-customElements.define('gs-tooltip', Tooltip); // note: name has to be two words
-
-/**
- * custom components lifeCycles while creating the custom components
- * executes in the following order:
- *    1. constructor()  -- Basic Initialization, wrong place for accessing the DOM \
- *                          as custom element is not been added to the DOM yet
- *    2. connectedCallback() -- this is called when custom element is connected to the DOM
- *    3. disconnectedCallback() -- this will be called when custom element will be detached from the DOM
- *    4. attributeChangedCallback() -- this will be called when attributes will be changed
- *                                  -- used when updating Data + DOM
- *    
- */
+customElements.define('email-confirmation-message', EmailConfirmation);
