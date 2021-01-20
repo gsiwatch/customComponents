f7fd91bd85e6f0e8748c722fdf0f332f8d9e5216
gsiwatch <gaurish.siwatch@gmail.com>
2021-01-19 20:40:46 -0500

    shadowDOM and setting innerHTML

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**
+ * shadow DOM and html template example
+ *      for creating encapsulated custom components
+ */
 class EmailConfirmation extends HTMLElement {
     constructor() {
         super();
-        this.textValue = `We've sent you an email from no-reply@RocketAccount.com
+        this.attachShadow({ mode: 'open'});
+        this.textValue = `We've sent you an email from <a href="">no-reply@RocketAccount.com</a>
         so we can verify your email address. Just click the button in the email 
         to finish creating your account. If you don't receive the email, plase check your spam or junk folder.`;
     }
@@ -31,8 +16,8 @@ class EmailConfirmation extends HTMLElement {
             this.textValue = this.getAttribute('text');
         }
         const bodyMessage = document.createElement('p');
-        bodyMessage.textContent = this.textValue;
-        this.appendChild(bodyMessage);
+        bodyMessage.innerHTML = this.textValue;
+        this.shadowRoot.appendChild(bodyMessage); // append the child to shadowDOM instead of lightDOM
     }
 }
 
