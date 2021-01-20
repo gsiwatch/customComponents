42c4c064aae980e6ac50a96921ad6403ba205562
gsiwatch <gaurish.siwatch@gmail.com>
2021-01-19 19:51:41 -0500

    adding attribute functionality

--- index.js
+++ index.js
@@ -21,13 +21,17 @@
 class EmailConfirmation extends HTMLElement {
     constructor() {
         super();
+        this.textValue = `We've sent you an email from no-reply@RocketAccount.com
+        so we can verify your email address. Just click the button in the email 
+        to finish creating your account. If you don't receive the email, plase check your spam or junk folder.`;
     }
 
     connectedCallback() {
+        if (this.hasAttribute('text')) {
+            this.textValue = this.getAttribute('text');
+        }
         const bodyMessage = document.createElement('p');
-        bodyMessage.textContent = `We've sent you an email from no-reply@RocketAccount.com
-        so we can verify your email address. Just click the button in the email 
-        to finish creating your account. If you don't receive the email, plase check your spam or junk folder.`;
+        bodyMessage.textContent = this.textValue;
         this.appendChild(bodyMessage);
     }
 }
